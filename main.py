5f62d2bf1aa7644a6e744f5377c7233349c8baf5
Ucis <98166442+Ucis-cmd@users.noreply.github.com>
2025-02-13 23:01:12 +0200

    adede hihglight

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import mpld3
 import numpy as np
 import pandas as pd
-from mpld3 import plugins
+from mpld3 import plugins, utils
 import matplotlib
 
 matplotlib.use("agg")
@@ -11,9 +11,38 @@ matplotlib.use("agg")
 app = Flask(__name__)
 
 
+class HighlightBarPlugin(plugins.PluginBase):
+    JAVASCRIPT = """
+    mpld3.register_plugin("highlightbar", HighlightBarPlugin);
+    HighlightBarPlugin.prototype = Object.create(mpld3.Plugin.prototype);
+    HighlightBarPlugin.prototype.constructor = HighlightBarPlugin;
+    HighlightBarPlugin.prototype.requiredProps = ["id"];
+    HighlightBarPlugin.prototype.defaultProps = {};
+
+    function HighlightBarPlugin(fig, props) {
+        mpld3.Plugin.call(this, fig, props);
+    };
+
+    HighlightBarPlugin.prototype.draw = function() {
+        var obj = mpld3.get_element(this.props.id);
+        console.log(obj);
+        var bars = obj.elements();
+        console.log(bars);
+
+        bars.on("mouseover", function(d, i) {
+            d3.select(this).style("fill", "orange");
+        }).on("mouseout", function(d, i) {
+            d3.select(this).style("fill", "skyblue");
+        });
+    };
+    """
+
+    def __init__(self, bar):
+        self.dict_ = {"type": "highlightbar", "id": utils.get_id(bar)}
+
+
 @app.route("/")
 def hello_world():
-
     categories = ["Apples", "Bananas", "Oranges", "Grapes", "Pineapples"]
     np.random.seed(42)
     values = np.random.randint(10, 100, size=len(categories))
@@ -21,6 +50,8 @@ def hello_world():
     fig, ax = plt.subplots()
 
     bars = ax.bar(categories, values, color="skyblue")
+
+    labels = []
 
     for i, (bar, category) in enumerate(zip(bars, categories)):
         height = bar.get_height()
@@ -32,6 +63,9 @@ def hello_world():
             va="top",
             fontsize=10,
         )
+        tooltip = plugins.LineLabelTooltip(bar, label=str(i))
+        highlight = HighlightBarPlugin(bar)
+        plugins.connect(plt.gcf(), tooltip, highlight)
 
     ax.set_xlabel("Fruits")
     ax.set_ylabel("Values")
@@ -63,7 +97,6 @@ def interactive_graph():
         label = df.iloc[[i], :].T
         label.columns = ["Row {0}".format(i)]
         target = round((df.iloc[i, 0]))
-        # .to_html() is unicode; so make leading 'u' go away with str()
         labels.append(str(label.to_html()))
         targets.append(f"/{target}")
 
