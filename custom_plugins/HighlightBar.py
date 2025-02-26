eacf03d6412bdf987c6808c4948e912efcb2f9a5
Ucis <98166442+Ucis-cmd@users.noreply.github.com>
2025-02-26 22:42:25 +0200

    Added pages for filtered dinosaur types and their diets

--- custom_plugins/HighlightBar.py
+++ custom_plugins/HighlightBar.py
@@ -6,7 +6,7 @@ class HighlightBarPlugin(plugins.PluginBase):
     mpld3.register_plugin("highlightbar", HighlightBarPlugin);
     HighlightBarPlugin.prototype = Object.create(mpld3.Plugin.prototype);
     HighlightBarPlugin.prototype.constructor = HighlightBarPlugin;
-    HighlightBarPlugin.prototype.requiredProps = ["id"];
+    HighlightBarPlugin.prototype.requiredProps = ["id", "link"];
     HighlightBarPlugin.prototype.defaultProps = {};
 
     function HighlightBarPlugin(fig, props) {
@@ -15,17 +15,21 @@ class HighlightBarPlugin(plugins.PluginBase):
 
     HighlightBarPlugin.prototype.draw = function() {
         var obj = mpld3.get_element(this.props.id);
-        console.log(obj);
-        var bars = obj.elements();
-        console.log(bars);
+        var bar = obj.elements();
 
-        bars.on("mouseover", function(d, i) {
+        // Capture the correct `this` context
+        var self = this;
+
+        bar.on("mouseover", function(d, i) {
             d3.select(this).style("fill", "orange");
         }).on("mouseout", function(d, i) {
             d3.select(this).style("fill", "skyblue");
+        }).on("click", function(d, i) {
+            // Use `self.props` instead of `this.props`
+            window.location.href = self.props.link;
         });
     };
     """
 
-    def __init__(self, bar):
-        self.dict_ = {"type": "highlightbar", "id": utils.get_id(bar)}
+    def __init__(self, bar, link):
+        self.dict_ = {"type": "highlightbar", "id": utils.get_id(bar), "link": link}
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import matplotlib.pyplot as plt
 import mpld3
 import numpy as np
@@ -6,14 +6,22 @@ import pandas as pd
 from mpld3 import plugins, utils
 import matplotlib
 from custom_plugins.HighlightBar import HighlightBarPlugin
+from custom_plugins.HighlightPie import HighlightPiePlugin
 from peewee import SqliteDatabase, fn
 from models import Dinosaur, db
 from data_conversions import db_to_csv, csv_to_db
-import os
+from werkzeug.urls import unquote
 
 matplotlib.use("agg")
 
 app = Flask(__name__)
+
+# whats left:
+# make the navbar links
+# make the website responsive (resize depending on screen size, might be hard because of the graphs, havent checked how to resize them, since the html is difficult to access)
+# make the huge blobs of dino names easier on the eyes (align them somehow, maybe make them bigger, or group them by letter, place each letter under a dropdown, etc.)
+# change the design of the graphs, select different colors, (check if the background can be made transparent?)
+# ADD MORE F_ING DINOSAUR GIFS (can be found in static/img folder)
 
 
 def init_db():
@@ -23,27 +31,27 @@ def init_db():
 
 @app.route("/")
 def homepage():
-    # get the unique values from 'type' column, place them in an array
-    # for each value in array, get the amount of values that are in the database
-    # and then make a bar graph from that
 
     query = (
-        Dinosaur.select(Dinosaur.type, fn.COUNT(Dinosaur.name).alias("count"))
+        Dinosaur.select(
+            Dinosaur.type, Dinosaur.link, fn.COUNT(Dinosaur.name).alias("count")
+        )
         .group_by(Dinosaur.type)
         .order_by(Dinosaur.type)
     )
     unique_types = []
     type_count = []
-    print(query)
+    links = []
     for item in query:
-        unique_types.append(item.type)
+        unique_types.append(item.type.capitalize())
         type_count.append(item.count)
+        links.append(f"/{item.type}")
 
     fig, ax = plt.subplots()
 
     bars = ax.bar(unique_types, type_count, color="skyblue")
 
-    for i, (bar, category) in enumerate(zip(bars, unique_types)):
+    for i, (bar, category, link) in enumerate(zip(bars, unique_types, links)):
         height = bar.get_height()
         ax.text(
             bar.get_x() + bar.get_width() / 2,
@@ -53,7 +61,7 @@ def homepage():
             va="top",
             fontsize=10,
         )
-        highlight = HighlightBarPlugin(bar)
+        highlight = HighlightBarPlugin(bar, link)
         plugins.connect(
             plt.gcf(), highlight
         )  # multiple plugins not working, update highlightbarplugin, if more functionality needed (just ask chatgpt or deepseek to do that)
@@ -69,6 +77,55 @@ def homepage():
         Html_file.write(html_str)
 
     return render_template("base.html")
+
+
+@app.route("/<type>")
+def type_page(type):
+    query = Dinosaur.select().where(Dinosaur.type == type)
+    query2 = (
+        Dinosaur.select(Dinosaur.diet, fn.COUNT(Dinosaur.name).alias("count"))
+        .where(Dinosaur.type == type)
+        .group_by(Dinosaur.diet)
+    )
+
+    dinosaurs = []
+    for item in query:
+        dinosaurs.append({"name": item.name, "link": item.link})
+
+    diets = []
+    diet_count = []
+    for item in query2:
+        diets.append(item.diet)
+        diet_count.append(item.count)
+
+    fig, ax = plt.subplots()
+    pie, texts, autotexts = ax.pie(diet_count, labels=diets, autopct="%1.1f%%")
+
+    for wedge, diet in zip(pie, diets):
+        plugins.connect(fig, HighlightPiePlugin(wedge, f"/{type}/{diet}"))
+
+    html_str = mpld3.fig_to_html(fig)
+    with open("./templates/diet_pie.html", "w") as Html_file:
+        Html_file.write(html_str)
+
+    return render_template(
+        "type_page.html",
+        type=type,
+        dinosaurs=dinosaurs,
+        unquote=unquote,  # unquote is used to convert url to the same format as request.path
+    )
+
+
+@app.route("/<type>/<diet>")
+def type_diet_page(type, diet):
+    query = Dinosaur.select().where(Dinosaur.type == type, Dinosaur.diet == diet)
+    dinosaurs = []
+    for item in query:
+        dinosaurs.append({"name": item.name, "link": item.link})
+
+    return render_template(
+        "type_page.html", type=type, diet=diet, dinosaurs=dinosaurs, unquote=unquote
+    )
 
 
 @app.route("/graph1")
